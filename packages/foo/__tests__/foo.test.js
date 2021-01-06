6e8bd8506f55a00a0ca9c32cfe060f89cb0f5899
bacikgoz <berk.acikgoz@hepsiburada.com>
2021-01-06 19:02:38 +0300

    built foo component

---
+++ packages/foo/__tests__/foo.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const foo = require('..');
+
+describe('@sleepybork/foo', () => {
+    it('needs tests');
+});
---
+++ packages/foo/lib/Foo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Foo = () => {
+    console.log("foo!");
+    return <div>Foo component</div>
+}
+
+export default Foo;
+
